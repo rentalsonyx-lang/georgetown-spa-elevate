ae8cde1a106cffa64bf337ab2298b9899d2ff4e6
rentalsonyx-lang <rentals.onyx@gmail.com>
2026-03-08 17:36:24 -0400

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import AnnouncementBar from "./AnnouncementBar";
 import Header from "./Header";
 import Footer from "./Footer";
 import FloatingCallButton from "./FloatingCallButton";
@@ -11,7 +10,6 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <AnnouncementBar />
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
